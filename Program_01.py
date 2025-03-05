3b7bc3fffc73ed6c321d58587d374b6d12b9ec60
Marsyus <miadolomboy0917@gmail.com>
2025-03-05 21:46:42 +0800

    Determine smaller number

--- Program_01.py
+++ Program_01.py
@@ -1,3 +1,4 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+print("\nThe smaller number is:", num_01 if num_01 < num_02 else num_02 if num_02 < num_01 else " ")
