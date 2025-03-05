d1428baefa046c77423cd170fd2933f848506872
Marsyus <miadolomboy0917@gmail.com>
2025-03-06 01:37:23 +0800

    Input two numbers

--- Program_10.py
+++ Program_10.py
@@ -1,1 +1,3 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+num_01 = int(input("Enter first number: "))
+num_02 = int(input("Enter second number: "))
