efa9bf5aa5eb0d02e0fc87674483905de8a548bd
Marsyus <miadolomboy0917@gmail.com>
2025-03-06 01:35:30 +0800

    Determine numbers not ending in 0 or 5

--- Program_09.py
+++ Program_09.py
@@ -1,4 +1,6 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 num = 0
 while num <= 100:
+    if num % 5:
+        print(num)
     num += 1
