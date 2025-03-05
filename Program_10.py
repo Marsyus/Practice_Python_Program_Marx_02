971bee88621cf127e369d695c966d350f7671186
Marsyus <miadolomboy0917@gmail.com>
2025-03-06 01:38:57 +0800

    Determine ascending numbers inbetween the two inputs

--- Program_10.py
+++ Program_10.py
@@ -1,3 +1,6 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+while num_01 < num_02 - 1:
+    num_01 += 1
+    print(num_01)
