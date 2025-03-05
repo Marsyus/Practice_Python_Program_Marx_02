e91a0f70c4ce6a3039240480f569d993104dba8c
Marsyus <miadolomboy0917@gmail.com>
2025-03-06 01:09:48 +0800

    Determine the result

--- Program_06.py
+++ Program_06.py
@@ -1,3 +1,5 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 for i in range(1, 11):
     num = int(input(f"Enter number {i}: "))
+    first_num = num if i == 1 else first_num - num
+print("\nThe result of the first number minus all of the remaining numbers is:", first_num)
