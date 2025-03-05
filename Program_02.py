39e4707eb16f79ebd48fb56e2b32b38c076beb77
Marsyus <miadolomboy0917@gmail.com>
2025-03-05 21:47:09 +0800

    Determine if not equal

--- Program_02.py
+++ Program_02.py
@@ -1,3 +1,4 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+print("\nThe two numbers are not equal." if num_01 != num_02 else " ")
