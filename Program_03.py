68443ecbbe21b4672e6fb2cca938a423e9dca1f5
Marsyus <miadolomboy0917@gmail.com>
2025-03-05 21:48:27 +0800

    Determine the difference

--- Program_03.py
+++ Program_03.py
@@ -1,3 +1,4 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+print("\nThe difference of the two numbers is:", num_01 - num_02)
