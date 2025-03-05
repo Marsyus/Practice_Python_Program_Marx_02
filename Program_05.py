75ba23539e62a315fb020d908af281163b640d45
Marsyus <miadolomboy0917@gmail.com>
2025-03-05 21:49:38 +0800

    Determine the remainder

--- Program_05.py
+++ Program_05.py
@@ -1,3 +1,4 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+print("\nThe remainder of the two when divided is: ", num_01 % num_02)
