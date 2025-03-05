8dbbc3ff8f7064316701b02006e997c1b497ecf3
Marsyus <miadolomboy0917@gmail.com>
2025-03-05 21:49:02 +0800

    Determine the quotient

--- Program_04.py
+++ Program_04.py
@@ -1,3 +1,4 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 num_01 = int(input("Enter first number: "))
 num_02 = int(input("Enter second number: "))
+print("\nThe quotient of the two numbers (without decimal) is: ", num_01 // num_02)
